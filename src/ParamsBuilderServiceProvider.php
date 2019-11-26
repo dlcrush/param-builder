af072bb304ece0dff9d92368391b401b86118ffb
David L. Crush <david.l.crush@disney.com>
2019-11-26 14:05:01 -0500

    add service provider

---
+++ src/ParamsBuilderServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Crush\Http;
+
+use Crush\Http\ParamsBuilder;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * ParamsBuilder Service Provider Class.
+ */
+class ParamsBuilderServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind('Crush\Http\Contracts\ParamsBuilder', function($app) {
+            return new Crush\Http\ParamsBuilder();
+        });
+    }
+
+}
